955a50e163b35fafa815adc51b75a4ac93d6b98c
Lucas Volle <Lucas@Volle.io>
2019-08-23 23:08:16 -0400

    Add eruda as content script 😕  but now it works 🙂

--- src/mobile-devtools.js
+++ src/mobile-devtools.js
@@ -1,17 +1,6 @@
-function OpenMobileDevtools()
-{
-	const erudaScript = document.createElement('script');
-	erudaScript.src = chrome.runtime.getURL('/node_modules/eruda/eruda.min.js');
-	erudaScript.onload = function() {
-		eruda.init();
-	}
-	document.head.appendChild(erudaScript);
-}
-
 let readyStateCheckInterval = setInterval(function() {
-if (document.readyState === 'complete') {
-	clearInterval(readyStateCheckInterval);
-
-	OpenMobileDevtools();
-}
-}, 10);
+	if (eruda) {
+		clearInterval(readyStateCheckInterval);
+		eruda.init()
+	}
+}, 100);
