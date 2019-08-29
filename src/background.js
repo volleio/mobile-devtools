d9136cdbb4b32169a0ad3b8ed8d67ffc383c1fad
Lucas Volle <lvolle@exagoinc.com>
2019-08-28 23:41:27 -0400

    Fix Google Analytics tracking

--- src/background.js
+++ src/background.js
@@ -1,14 +1,17 @@
-// Google Analytics initialization 
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-68030939-2');
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-68030939-2', 'auto');
+ga('set', 'checkProtocolTask', null); // Disables file protocol checking.
 
 
 chrome.browserAction.onClicked.addListener((tab) => {
     if (tab) {
         try {
             chrome.tabs.sendMessage(tab.id, { action: "toggle_devtools" });  
+            ga('send', 'pageview', '/popup'); // Set page, avoiding rejection due to chrome-extension protocol 
         }
         catch(err) { } // sendMessage will fail in extensions menu, new tab, etc. 
     }
