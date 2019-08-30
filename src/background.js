3aa876554a977cb79ecdcf19ad428513af6291b5
Lucas Volle <Lucas@Volle.io>
2019-08-29 22:53:38 -0400

    Remove content script to minimize extension permissions, use tabs.executeScript instead to inject initial script. Update manifest to be CWS ready.

--- src/background.js
+++ src/background.js
@@ -10,8 +10,13 @@ ga('set', 'checkProtocolTask', null); // Disables file protocol checking.
 chrome.browserAction.onClicked.addListener((tab) => {
     if (tab) {
         try {
-            chrome.tabs.sendMessage(tab.id, { action: "toggle_devtools" });  
-            ga('send', 'pageview', '/popup'); // Set page, avoiding rejection due to chrome-extension protocol 
+            chrome.tabs.sendMessage(tab.id, { action: "toggle_devtools" }, (response) => {
+                if (!response || !response.success) {
+                    chrome.runtime.lastError.message; // literally just ackknowledge that the call failed so Chrome doesn't log it
+                    chrome.tabs.executeScript(tab.id, { file: 'mobile-devtools.js' });
+                    ga('send', 'pageview', '/popup'); // Indicate initial execution, avoiding rejection due to chrome-extension protocol 
+                }
+            });  
         }
         catch(err) { } // sendMessage will fail in extensions menu, new tab, etc. 
     }
--- src/mobile-devtools.js
+++ src/mobile-devtools.js
@@ -1,73 +1,77 @@
-let loadedEruda = false;
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	if (request.action === "toggle_devtools") {
-		if (!loadedEruda) {
-			// Create loading spinner
-			addLoadingCss();
-			const loadingSpinner = document.createElement('div');
-			loadingSpinner.id = 'mobile-devtools-loader';
-			loadingSpinner.className = 'mobile-devtools-loader';
-			document.body.appendChild(loadingSpinner);
-			
-			addScriptToHead('eruda.min.js');
-			loadedEruda = true;
-		}
+		sendResponse({ success: true });
+		
+		if (document.getElementById('toggle-devtools.js'))
+			document.getElementById('toggle-devtools.js').remove();
+		
 		addScriptToHead('toggle-devtools.js');
 	}
 });
 
+addLoadingSpinner();
+addScriptToHead('eruda.min.js');
+addScriptToHead('toggle-devtools.js');
+
+
 function addScriptToHead(url) {
 	const script = document.createElement('script');
+	script.id = url;
 	script.src = chrome.extension.getURL(url);
 	(document.head||document.documentElement).appendChild(script);
 }
 
-function addLoadingCss() {
+function addLoadingSpinner() {
 	const styles = document.createElement('style');
 	(document.head||document.documentElement).appendChild(styles);
 	styles.innerHTML = `
-	.mobile-devtools-loader,
-	.mobile-devtools-loader:before,
-	.mobile-devtools-loader:after {
-		position: fixed;
-		bottom: 100px;
-		margin-left: 50%;
-		margin-right: 50%;
-		width: 10px;
-		height: 40px;
-		z-index: 999999999;
-		background: #2196f3;
-		animation: mobile-devtool-loading 1s infinite ease-in-out;
-	}
-	.mobile-devtools-loader {
-		color: #2196f3;
-		transform: translateZ(0);
-		animation-delay: -0.16s;
-	}
-	.mobile-devtools-loader:before,
-	.mobile-devtools-loader:after {
-		position: absolute;
-		top: 0;
-		content: '';
-	}
-	.mobile-devtools-loader:before {
-		left: -25px;
-		animation-delay: -0.32s;
-	}
-	.mobile-devtools-loader:after {
-	  	left: 15px;
-	}
-	@keyframes mobile-devtool-loading {
-		0%,
-		80%,
-		100% {
-			box-shadow: 0 0;
+		.mobile-devtools-loader,
+		.mobile-devtools-loader:before,
+		.mobile-devtools-loader:after {
+			position: fixed;
+			bottom: 100px;
+			margin-left: 50%;
+			margin-right: 50%;
+			width: 10px;
 			height: 40px;
+			z-index: 999999999;
+			background: #2196f3;
+			animation: mobile-devtool-loading 1s infinite ease-in-out;
 		}
-		40% {
-			box-shadow: 0 -20px;
-			height: 50px;
+		.mobile-devtools-loader {
+			color: #2196f3;
+			transform: translateZ(0);
+			animation-delay: -0.16s;
 		}
-	}
+		.mobile-devtools-loader:before,
+		.mobile-devtools-loader:after {
+			position: absolute;
+			top: 0;
+			content: '';
+		}
+		.mobile-devtools-loader:before {
+			left: -25px;
+			animation-delay: -0.32s;
+		}
+		.mobile-devtools-loader:after {
+			left: 15px;
+		}
+		@keyframes mobile-devtool-loading {
+			0%,
+			80%,
+			100% {
+				box-shadow: 0 0;
+				height: 40px;
+			}
+			40% {
+				box-shadow: 0 -20px;
+				height: 50px;
+			}
+		}
 	`;
+
+	const loadingSpinner = document.createElement('div');
+	loadingSpinner.id = 'mobile-devtools-loader';
+	loadingSpinner.className = 'mobile-devtools-loader';
+	document.body.appendChild(loadingSpinner);
 }
